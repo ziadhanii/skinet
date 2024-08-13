a845abf0daa379d8de35224efee138d70813c9c3
Ziad Hany <UG_31107103@ics.tanta.edu.eg>
2024-08-14 02:13:05 +0300

    Initial commit: Project setup, Product entity, and Products controller

---
+++ Infrastructure/Config/ProductConfigration.cs
@@ -0,0 +1,13 @@
+using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+namespace Infrastructure.Config;
+
+public class ProductConfigration : IEntityTypeConfiguration<Product>
+{
+    public void Configure(EntityTypeBuilder<Product> builder)
+    {
+        builder.Property(x => x.Price).HasColumnType("decimal(18,2)");
+        builder.Property(x => x.Name).IsRequired();
+    }
+}
---
+++ Infrastructure/Data/StoreContext.cs
@@ -0,0 +1,22 @@
+using Core.Entities;
+using Infrastructure.Config;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data
+{
+    public class StoreContext(DbContextOptions<StoreContext> options) : DbContext(options)
+    {
+        public DbSet<Product> Products { get; set; }
+
+
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ProductConfigration).Assembly);
+
+        }
+    }
+}
